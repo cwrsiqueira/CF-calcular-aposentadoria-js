337f6ef061a000d5f77df3c4a1bb4cfc4b9b5ce3
Carlos Wagner <cwrsiqueira@msn.com>
2024-11-30 01:30:46 -0300

    Encontrado bug

--- script.js
+++ script.js
@@ -477,7 +477,7 @@ if (document.getElementById("copiarBtn")) {
     ).textContent;
 
     // Formata o resultado para ser copiado
-    const resultado = `Calcular Aposentadoria teste
+    const resultado = `Calcular Aposentadoria
 https://www.calcularaposentadoria.com
 
 Resultados
